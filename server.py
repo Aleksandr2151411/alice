717862bdd99faf4601325a0ec758d5fe8bc10cfd
Aleksandr <knuckles05@yandex.ru>
2024-04-10 17:22:15 +0300

    bbb

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import logging
 from flask import Flask, request, jsonify
+import json
+import random
 
 
 app = Flask(__name__)
@@ -7,36 +9,70 @@ logging.basicConfig(level=logging.INFO)
 
 storageSession = {}
 
+cities = {
+    'Калуга': ['1540737/8c32f088708e5d2464ba'],
+    'Бердичев': ['1030494/56dcac2ca4e040f4678c'],
+    'Санкт-Петербург': ['1521359/2982f6417250cfba7242']
+}
 
-def get_suggests(user_id):
-    session = storageSession[user_id]
-    suggests = [{'title': suggest, 'hide': True}
-                for suggest in session['suggests'][:2]]
-    session['suggests'] = session['suggests'][1:]
-    storageSession[user_id] = session
-    if len(suggests) < 2:
-        suggests.append({
-            'title': "Ладно", "url": "https://eda.yandex.ru/moscow?shippingType=delivery", "hide": True
-        })
-    return suggests
+# def get_suggests(user_id):
+#     session = storageSession[user_id]
+#     suggests = [{'title': suggest, 'hide': True}
+#                 for suggest in session['suggests'][:2]]
+#     session['suggests'] = session['suggests'][1:]
+#     storageSession[user_id] = session
+#     if len(suggests) < 2:
+#         suggests.append({
+#             'title': "Ладно", "url": "https://eda.yandex.ru/moscow?shippingType=delivery", "hide": True
+#         })
+#     return suggests
+
+
+def get_first_name(request):
+    for entity in request['request']['nlu']['entities']:
+        if entity['type'] == "YANDEX.FIO":
+            return entity['value'].get('first_name', None)
+
+
+def get_city(request):
+    for entity in request['request']['nlu']['entities']:
+        if entity['type'] == "YANDEX.GEO":
+            return entity['value'].get('city', None)
 
 
 def handle_dialog(request, response):
     user_id = request['session']['user_id']
     if request['session']['new']:
-        storageSession[user_id] = {
-            'suggests': ["Не хочу", "Не буду", "Отстань", "Чего?", "Кого?"]
-        }
-        response['response']['text'] = 'Привет, купи слона!'
-        response['response']['buttons'] = get_suggests(user_id)
+        response['response']['text'] = 'Привет! Как тебя зовут?'
+        storageSession[user_id] = {'first_name': None}
         return
-    if request['session']['original_utterance'].lower() in ["ладно", "куплю", "покупаю", "хорошо", "уже купил"]:
-        response['response']['text'] = "А еще слона можно заказать на Яндекс.еда!"
-        response['response']['end_session'] = True
-        return
-    response['response']['text'] = \
-        f"Все говорят '{request['session']['original_utterance'].lower()}', a ты возьми и купи слона!"
-    response['response']['button'] = get_suggests(user_id)
+    if storageSession[user_id]['first_name'] is None:
+        first_name = get_first_name(request)
+        if first_name is None:
+            response['response']['text'] = 'Не слышу, пожалуйста повторите'
+        else:
+            storageSession[user_id]['first_name'] = first_name
+            response['response']['text'] = (f'Приятно познакомиться, {first_name.title()}',
+                                            f'Меня зовут Алиса',
+                                            f'Какой город показать?')
+            response['response']['buttons'] = [
+                {
+                    'title': city.title(),
+                    'hide': True
+                }
+                for city in cities
+            ]
+    else:
+        city = get_city(request)
+        if city in cities:
+            response['response']['card'] = {}
+            response['response']['card']['type'] = 'BigImage'
+            response['response']['card']['title'] = 'Этот город я знаю'
+            response['response']['card']['image_id'] = random.choice(cities[city])
+            response['response']['text'] = 'Я угадал'
+        else:
+            response['response']['text'] = ('Первый раз слышу такой город'
+                                            'Давай ещё')
 
 
 @app.route('/post', methods=['POST'])
